7ff74cdfb57ca9c36e02143a21879d34d245cd05
Marc Reyes-Maninang <marc.maninang@gmail.com>
2020-07-17 10:44:38 -0700

    Added full width template

---
+++ page-templates/full-width.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Genesis Sample.
+ *
+ * This file adds the landing page template to the Genesis Sample Theme.
+ *
+ * Template Name: Full-Width
+ *
+ * @package Genesis Sample
+ * @author  StudioPress
+ * @license GPL-2.0-or-later
+ * @link    https://www.studiopress.com/
+ */
+
+add_filter( 'body_class', 'genesis_full_width_class' );
+/**
+ * Adds landing page body class.
+ *
+ * @since 1.0.0
+ *
+ * @param array $classes Original body classes.
+ * @return array Modified body classes.
+ */
+function genesis_full_width_class( $classes ) {
+
+	$classes[] = 'full-width-content';
+	return $classes;
+
+}
+
+// Runs the Genesis loop.
+genesis();
