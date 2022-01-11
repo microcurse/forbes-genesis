6cb077b2dacf9da0abc7ad3c82ea60543aa324cb
Marc <marc.maninang@gmail.com>
2022-01-11 09:49:34 -0800

    Increased products per page

--- functions.php
+++ functions.php
@@ -459,3 +459,16 @@ function wpse_remove_edit_post_link( $link ) {
 	return '';
 }
 add_filter('edit_post_link', 'wpse_remove_edit_post_link');
+
+
+/**
+ * Change number of products that are displayed per page (shop page)
+ */
+add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 20 );
+
+function new_loop_shop_per_page( $cols ) {
+  // $cols contains the current number of products per page based on the value stored on Options –> Reading
+  // Return the number of products you wanna show per page.
+  $cols = 24;
+  return $cols;
+}
