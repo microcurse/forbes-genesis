0e3622993df00d67464ebb5d0053f9efcb6fa7b1
Marc <marc.maninang@gmail.com>
2022-01-25 11:35:24 -0800

    Update page template with full width banner. More fixes to WooCommerce Blocks

--- page-templates/default.php
+++ page-templates/default.php
@@ -11,7 +11,7 @@
 
 add_filter( 'body_class', 'genesis_full_width_class' );
 /**
- * Adds landing page body class.
+ * Adds page body class.
  *
  * @since 1.0.0
  *
@@ -22,6 +22,61 @@ function genesis_full_width_class( $classes ) {
 
 	$classes[] = 'full-width-content';
 	return $classes;
+
+}
+
+// Remove current header
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+
+// Add current header
+add_action( 'genesis_after_header', 'genesis_entry_header_markup_open', 5 );
+add_action( 'genesis_after_header', 'fi_entry_header_wrap_open', 6 );
+add_action( 'genesis_after_header', 'genesis_do_post_title' );
+add_action( 'genesis_after_header', 'genesis_post_info' );
+add_action( 'genesis_after_header', 'fi_do_entry_header_bg', 13);
+add_action( 'genesis_after_header', 'fi_entry_header_wrap_close', 14 );
+add_action( 'genesis_after_header', 'genesis_entry_header_markup_close', 15 );
+
+// ACF Add background image to archive header
+function fi_do_entry_header_bg() {
+  $term = get_queried_object();
+  $image = get_field('page_header_background', $term);
+
+  ?>
+  <style type="text/css">
+	
+    <?php if( $image ): ?>
+    .entry-header {
+      background-image: url(<?php echo $image['url']; ?>);
+    }
+    <?php endif; ?>
+    
+  </style>
+  <?php
+}
+
+function fi_entry_header_wrap_open() {
+    
+	genesis_markup(
+			[
+					'open'      => '<div %s>',
+					"context"   => 'wrap',
+			]
+	);
+
+}
+
+function fi_entry_header_wrap_close() {
+
+	genesis_markup(
+			[
+					'open'      => '</div>',
+					"context"   => 'wrap',
+			]
+	);
 
 }
 
