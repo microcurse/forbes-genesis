ad99de3b2f2d2b89786d66603260ec85b7d60575
Marc <marc.maninang@gmail.com>
2022-02-03 15:12:30 -0800

    Update category header bg images with size: cover and no-repeat

--- woocommerce/taxonomy.php
+++ woocommerce/taxonomy.php
@@ -172,6 +172,8 @@ function fi_genesis_do_archive_background() {
     <?php if( $image ): ?>
     .archive-description {
       background-image: url(<?php echo $image['url']; ?>);
+      background-size: cover;
+      background-repeat: no-repeat;
     }
     <?php endif; ?>
     
